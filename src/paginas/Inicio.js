adf0a1e9776362aa03d8bf137dde761506d02f41
gerald-gv <geraldgutierrez918@gmail.com>
2025-06-09 22:55:30 -0500

    Fecha y hora agregada

--- src/paginas/Inicio.js
+++ src/paginas/Inicio.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavMenu from "../elementos/NavMenu"
 import SliderImg from "../elementos/Slider"
+import Fecha from '../elementos/Fecha';
+import Hora from '../elementos/Hora';
 import SectionLibros from "../elementos/Section-Libros"
 import SectionInicio from "../elementos/Section-Inicio"
 import Footer from "../elementos/Footer"
@@ -10,6 +12,10 @@ const Inicio = () => {
   return (
     <div>
         <NavMenu />
+        <div className="info-datetime">
+            <Fecha />
+            <Hora />
+          </div>
         <main>
             <SliderImg />
             <h2 className="titulo-slider">Libros Destacados</h2>
