218eaacee652a2ba18a8dbfb8fd1e0a85657245f
gerald-gv <geraldgutierrez918@gmail.com>
2025-06-25 19:36:41 -0500

    Utilizando APIs de Strapi - Validando usuarios

--- src/usuario/UsuarioContext.js
+++ src/usuario/UsuarioContext.js
@@ -4,49 +4,17 @@ export const UsuarioContext = createContext();
 
 export const UsuarioGlobal = ({ children }) => {
   const [usuario, setUsuario] = useState(null);
-  const [usuariosBD, setUsuariosBD] = useState([]);
 
   useEffect(() => {
-    const cargarUsuarios = async () => {
-      try {
-        //Cargar usuarios predefinidos desde usuarios.json
-        const respuesta = await fetch("/usuarios.json");
-        const usuariosJSON = await respuesta.json();
-
-        //Leer usuarios guardados desde el localStorage
-        const usuariosLocales = JSON.parse(localStorage.getItem("usuarios")) || [];
-
-        //Combinar los usuarios del JSON y los registrados localmente
-        const usuariosGlobales = [...usuariosJSON, ...usuariosLocales];
-
-        // Establecemos los usuariosglobales para guardarlos en UsuariosBD
-        setUsuariosBD(usuariosGlobales);
-
-        //Verificar si hay un usuario logueado guardado
-        const usuarioActual = JSON.parse(localStorage.getItem("usuario"));
-        if (usuarioActual) {
-          const usuarioValido = usuariosGlobales.find(
-            u => u.email === usuarioActual.email && u.contraseña === usuarioActual.contraseña
-          );
-
-          if (usuarioValido)
-            setUsuario(usuarioValido);
-          else localStorage.removeItem("usuario");
-        }
-      } catch (error) {
-        console.error("Error al cargar usuarios", error);
-      }
-    };
-
-    cargarUsuarios();
+    // Cargar sesión persistente globalmente
+    const usuarioGuardado = JSON.parse(localStorage.getItem("usuario"));
+    if (usuarioGuardado) {
+      setUsuario(usuarioGuardado);
+    }
   }, []);
 
-  useEffect(() => {
-    console.log('usuarios', usuariosBD)
-  },[usuariosBD])
-  
   return (
-    <UsuarioContext.Provider value={{ usuario, setUsuario, usuariosBD, setUsuariosBD }}>
+    <UsuarioContext.Provider value={{ usuario, setUsuario }}>
       {children}
     </UsuarioContext.Provider>
   );
